3d41c9c5ca853e3b3a3fe006ea3100fb8fdec6d6
一路踩红 <346551572@qq.com>
2020-04-27 18:18:15 +0800

    ''

--- src/views/pages/index.vue
+++ src/views/pages/index.vue
@@ -26,12 +26,26 @@
           </li>
           <li>
             <span>红米</span>
-            <div class="index-nav_sublist"></div>
+            <div class="index-nav_sublist">
+              <div class="container">
+                <ul>
+                  <li class="index-nav_prod" v-for="item in productListTwo" :key="item.id">
+                    <a :href="'/#/product/'+item.id" target="_blank">
+                      <div class="prod-img">
+                        <img :src="item.mainImage" :alt="item.subtitle">
+                      </div>
+                      <div class="prod-name">{{item.name}}</div>
+                      <div class="prod-price">{{item.price | priceFormatter}}</div>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </li>
-          <li>
-            <span>电视</span>
-            <div class="index-nav_sublist"></div>
-          </li>
+          <!--          <li>-->
+          <!--            <span>电视</span>-->
+          <!--            <div class="index-nav_sublist"></div>-->
+          <!--          </li>-->
         </ul>
         <div class="index-nav_search">
           <div class="wrapper">
@@ -41,20 +55,298 @@
           </a>
         </div>
       </div>
+      <div class="index-swiper" style="position: relative">
+        <div class="index-swiper_menu">
+          <div class="index-swiper_menuItem">
+            <div class="index-swiper_toggle">手机卡 电话卡</div>
+            <div class="subListCon">
+              <div class="index-swiper_subList" v-for="(item, index) in menuList" :key="index">
+                <div class="index-swiper_subItem" v-for="subItem in item" :key="subItem ? subItem.id : Math.random()">
+                  <a :href="subItem ? '/#/product/' + subItem.id : ''">
+                    <img :src="subItem.img || require('../../../public/imgs/item-box-1.png')"
+                         :alt="subItem.name || '小米9'">
+                    <div>{{subItem.name || '小米9'}}</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="index-swiper_menuItem">
+            <div class="index-swiper_toggle">电视 盒子</div>
+            <div class="subListCon">
+              <div class="index-swiper_subList" v-for="(item, index) in menuList" :key="index">
+                <div class="index-swiper_subItem" v-for="subItem in item" :key="subItem ? subItem.id : Math.random()">
+                  <a :href="subItem ? '/#/product/' + subItem.id : ''">
+                    <img :src="subItem.img || require('../../../public/imgs/item-box-1.png')"
+                         :alt="subItem.name || '小米9'">
+                    <div>{{subItem.name || '小米9'}}</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="index-swiper_menuItem">
+            <div class="index-swiper_toggle">单车</div>
+            <div class="subListCon">
+              <div class="index-swiper_subList" v-for="(item, index) in menuList" :key="index">
+                <div class="index-swiper_subItem" v-for="subItem in item" :key="subItem ? subItem.id : Math.random()">
+                  <a :href="subItem ? '/#/product/' + subItem.id : ''">
+                    <img :src="subItem.img || require('../../../public/imgs/item-box-1.png')"
+                         :alt="subItem.name || '单车'">
+                    <div>{{subItem.name || '单车'}}</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="index-swiper_menuItem">
+            <div class="index-swiper_toggle">电源配件</div>
+            <div class="subListCon">
+              <div class="index-swiper_subList" v-for="(item, index) in menuList" :key="index">
+                <div class="index-swiper_subItem" v-for="subItem in item" :key="subItem ? subItem.id : Math.random()">
+                  <a :href="subItem ? '/#/product/' + subItem.id : ''">
+                    <img :src="subItem.img || require('../../../public/imgs/item-box-1.png')"
+                         :alt="subItem.name || '电源配件'">
+                    <div>{{subItem.name || '电源配件'}}</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="index-swiper_menuItem">
+            <div class="index-swiper_toggle">笔记本</div>
+            <div class="subListCon">
+              <div class="index-swiper_subList" v-for="(item, index) in menuList" :key="index">
+                <div class="index-swiper_subItem" v-for="subItem in item" :key="subItem ? subItem.id : Math.random()">
+                  <a :href="subItem ? '/#/product/' + subItem.id : ''">
+                    <img :src="subItem.img || require('../../../public/imgs/item-box-1.png')"
+                         :alt="subItem.name || '小米9'">
+                    <div>{{subItem.name || '小米9'}}</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="index-swiper_menuItem">
+            <div class="index-swiper_toggle">笔记本</div>
+            <div class="subListCon">
+              <div class="index-swiper_subList" v-for="(item, index) in menuList" :key="index">
+                <div class="index-swiper_subItem" v-for="subItem in item" :key="subItem ? subItem.id : Math.random()">
+                  <a :href="subItem ? '/#/product/' + subItem.id : ''">
+                    <img :src="subItem.img || require('../../../public/imgs/item-box-1.png')"
+                         :alt="subItem.name || '小米9'">
+                    <div>{{subItem.name || '小米9'}}</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="index-swiper_menuItem">
+            <div class="index-swiper_toggle">笔记本</div>
+            <div class="subListCon">
+              <div class="index-swiper_subList" v-for="(item, index) in menuList" :key="index">
+                <div class="index-swiper_subItem" v-for="subItem in item" :key="subItem ? subItem.id : Math.random()">
+                  <a :href="subItem ? '/#/product/' + subItem.id : ''">
+                    <img :src="subItem.img || require('../../../public/imgs/item-box-1.png')"
+                         :alt="subItem.name || '小米9'">
+                    <div>{{subItem.name || '小米9'}}</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="index-swiper_menuItem">
+            <div class="index-swiper_toggle">家电</div>
+            <div class="subListCon">
+              <div class="index-swiper_subList" v-for="(item, index) in menuList" :key="index">
+                <div class="index-swiper_subItem" v-for="subItem in item" :key="subItem ? subItem.id : Math.random()">
+                  <a :href="subItem ? '/#/product/' + subItem.id : ''">
+                    <img :src="subItem.img || require('../../../public/imgs/item-box-1.png')"
+                         :alt="subItem.name || '家电'">
+                    <div>{{subItem.name || '家电'}}</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="index-swiper_menuItem">
+            <div class="index-swiper_toggle">路由器</div>
+            <div class="subListCon">
+              <div class="index-swiper_subList" v-for="(item, index) in menuList" :key="index">
+                <div class="index-swiper_subItem" v-for="subItem in item" :key="subItem ? subItem.id : Math.random()">
+                  <a :href="subItem ? '/#/product/' + subItem.id : ''">
+                    <img :src="subItem.img || require('../../../public/imgs/item-box-1.png')"
+                         :alt="subItem.name || '路由器'">
+                    <div>{{subItem.name || '路由器'}}</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="index-swiper_menuItem">
+            <div class="index-swiper_toggle">笔记本</div>
+            <div class="subListCon">
+              <div class="index-swiper_subList" v-for="(item, index) in menuList" :key="index">
+                <div class="index-swiper_subItem" v-for="subItem in item" :key="subItem ? subItem.id : Math.random()">
+                  <a :href="subItem ? '/#/product/' + subItem.id : ''">
+                    <img :src="subItem.img || require('../../../public/imgs/item-box-1.png')"
+                         :alt="subItem.name || '笔记本'">
+                    <div>{{subItem.name || '笔记本'}}</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <swiper ref="mySwiper" :options="swiperOptions">
+          <swiper-slide v-for="item in slideList" :key="item.id">
+            <img :src=item.img alt="slideImg"/>
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+      </div>
+      <div class="index-adsList">
+        <a class="index-adsList_item" v-for="item in adsList" :key="item.id">
+          <img :src="item.img" alt="advertise"/>
+        </a>
+      </div>
     </div>
+    <div class="index-phone">
+      <div class="container">
+        <h5>手机</h5>
+        <div class="index-phone_wrapper">
+          <a href="/#/product/111" class="index-phone_alpha">
+            <img src="../../../public/imgs/mix-alpha.jpg" alt="mi mix alpha">
+          </a>
+          <div class="index-phone_list">
+            <div class="index-phone_item" v-for="item in phoneList" :key="item.id">
+              <span class="index-phone_tag" v-if="item.status">新品</span>
+              <div class="index-phone_imgWrapper">
+                <img :src="item.mainImage" :alt="item.name"/>
+              </div>
+              <div class="index-phone_name">{{item.name}}</div>
+              <div class="index-phone_desc">{{item.subtitle}}</div>
+              <div class="index-phone_price">
+                {{item.price}}
+                <div class="index-phone_buy"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <service-bar></service-bar>
   </div>
 </template>
 
 <script>
+import ServiceBar from '../components/ServiceBar'
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
 export default {
   name: 'index',
   data () {
     return {
-      productList: []
+      productList: [],
+      productListTwo: [],
+      productListThree: [],
+      slideList: [
+        {
+          id: '42',
+          img: require('../../../public/imgs/slider/slide-1.jpg')
+        },
+        {
+          id: '45',
+          img: require('../../../public/imgs/slider/slide-2.jpg')
+        },
+        {
+          id: '46',
+          img: require('../../../public/imgs/slider/slide-3.jpg')
+        },
+        {
+          id: '47',
+          img: require('../../../public/imgs/slider/slide-4.jpg')
+        },
+        {
+          id: '48',
+          img: require('../../../public/imgs/slider/slide-1.jpg')
+        }
+      ],
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        effect: 'cube',
+        cubeEffect: {
+          slideShadows: false,
+          shadow: false,
+          shadowOffset: 100,
+          shadowScale: 0.6
+        },
+        loop: true
+      },
+      menuList: [
+        [
+          {
+            id: 30,
+            img: require('../../../public/imgs/item-box-1.png'),
+            name: '小米CC9'
+          },
+          {
+            id: 31,
+            img: require('../../../public/imgs/item-box-2.png'),
+            name: '小米8青春版'
+          },
+          {
+            id: 32,
+            img: require('../../../public/imgs/item-box-3.jpg'),
+            name: 'Redmi K20 Pro'
+          },
+          {
+            id: 33,
+            img: require('../../../public/imgs/item-box-4.jpg'),
+            name: '移动4G专区'
+          }
+        ],
+        [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]
+      ],
+      adsList: [
+        {
+          id: 33,
+          img: require('../../../public/imgs/ads/ads-1.png')
+        },
+        {
+          id: 48,
+          img: require('../../../public/imgs/ads/ads-2.jpg')
+        },
+        {
+          id: 45,
+          img: require('../../../public/imgs/ads/ads-3.png')
+        },
+        {
+          id: 47,
+          img: require('../../../public/imgs/ads/ads-4.jpg')
+        }
+      ],
+      phoneList: []
     }
+  },
+  components: {
+    ServiceBar,
+    Swiper,
+    SwiperSlide
   },
   created () {
     this.getProductList()
+    this.getPhoneList()
   },
   filters: {
     priceFormatter (val) {
@@ -65,8 +357,14 @@ export default {
   methods: {
     getProductList () {
       this.$apis.productList(100012).then(res => {
-        console.log(res)
         this.productList = res.data.list.slice(0, 6)
+        this.productListTwo = res.data.list.slice(5)
+      })
+    },
+    getPhoneList () {
+      this.$apis.productList(100012).then(res => {
+        console.log(res)
+        this.phoneList = res.data.list.slice(0, 8)
       })
     },
     toLogin () {
@@ -249,6 +547,189 @@ export default {
           background: url("../../../public/imgs/icon-search.png") no-repeat center;
           background-size: contain;
         }
+      }
+    }
+  }
+
+  .index-swiper {
+    .swiper-container {
+      img {
+        height: 460px;
+      }
+    }
+
+    .index-swiper_menu {
+      position: absolute;
+      left: 0;
+      top: 0;
+      padding: 20px 0;
+      z-index: 99999;
+      color: #fff;
+      background-color: rgba(105, 101, 101, .6);;
+
+      .index-swiper_menuItem {
+        .index-swiper_toggle {
+          width: 234px;
+          height: 42px;
+          line-height: 42px;
+          cursor: pointer;
+          padding-left: 30px;
+        }
+
+        &:hover {
+          .index-swiper_toggle {
+            background-color: $colorA;;
+          }
+
+          .subListCon {
+            display: block !important;
+          }
+        }
+
+        .subListCon {
+          display: none;
+          position: absolute;
+          left: 264px;
+          top: 0;
+          z-index: 99999;
+
+          .index-swiper_subList {
+            width: 992px;
+            background-color: #fff;
+
+            &::after, &::before {
+              content: '';
+              display: block;
+              clear: both;
+            }
+
+            .index-swiper_subItem {
+              box-sizing: border-box;
+              float: left;
+              width: 25%;
+              padding: 18px 20px;
+
+              a {
+                img, div {
+                  display: inline-block;
+                  vertical-align: middle;
+                }
+
+                img {
+                  width: 35px;
+                  height: 42px;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .index-adsList {
+    &::before, &::after {
+      content: '';
+      display: block;
+      clear: both;
+    }
+
+    .index-adsList_item:not(:first-child) {
+      margin-left: 14px;
+    }
+
+    .index-adsList_item {
+      float: left;
+
+      img {
+        width: 296px;
+        height: 167px;
+      }
+    }
+  }
+
+  .index-phone {
+    padding-top: 30px;
+    background-color: #f5f5f5;
+
+    h5 {
+      font-size: 22px;
+      height: 21px;
+      line-height: 21px;
+      color: #333;
+      margin-bottom: 20px;
+    }
+
+    .index-phone_alpha {
+      display: inline-block;
+
+      img {
+        width: 224px;
+        height: 619px;
+      }
+    }
+
+    .index-phone_list {
+      display: inline-block;
+      width: 986px;
+      padding-left: 16px;
+
+      &::before, &::after {
+        content: '';
+        display: block;
+        clear: both;
+      }
+
+      .index-phone_item {
+        float: left;
+        width: 236px;
+        height: 302px;
+        background-color: #fff;
+        text-align: center;
+
+        .index-phone_tag {
+          display: inline-block;
+          width: 67px;
+          height: 24px;
+          line-height: 24px;
+          font-size: 14px;
+          color: #fff;
+          background-color: #7ecf68;
+        }
+
+        .index-phone_imgWrapper {
+          img {
+            width: 100%;
+            height: 195px;
+          }
+        }
+      }
+
+      .index-phone_item:not(:nth-child(1)):not(:nth-child(5)) {
+        margin-left: 14px;
+      }
+
+      .index-phone_item:not(:nth-child(n+5)) {
+        margin-bottom: 14px;
+      }
+
+      .index-phone_name {
+        font-size: 14px;
+        color: #333;
+        line-height: 14px;
+        font-weight: 700;
+      }
+
+      .index-phone_desc {
+        color: #999;
+        line-height: 13px;
+        margin: 6px auto 13px;
+      }
+
+      .index-phone_price {
+        color: #f20a0a;
+        font-size: 14px;
+        font-weight: 700;
       }
     }
   }
